1ffbef5b7f3c58590ea07bafd02e97a47d1c289a
anthonyabsher <anthonyabsher@gmail.com>
2016-05-16 20:25:30 -0700

    create controller for home screen, view for home sceen sets

---
+++ app/client/js/controllers/homeScreenController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+angular.module('studybuddyApp')
+.controller('homeScrnCntrl',[$scope, $rootScope, $http,
+		function($scope, $rootScope, $http){
+
+			$scope.getSets = function(){
+				$http.get('/sets').success(function(data,status,headers,config){
+					$scope.results = data;
+
+					var setResults = [];
+					for(var i = 0; i < data.length; i++){
+						setResults[i] = { "Name": data[i].Name, "Category": data[i].Category, 
+						"Author": data[i].Author}
+					}
+					$rootScope.setResults = setResults;
+				});
+			};
+		}
+	]);
