64e590b1ec44b5cea343d9d29bc9f54e16622c4e
dipaliv <vagal.dipali@gmail.com>
2016-05-24 10:02:13 -0700

    updated code to print flashcard set details on the card.html file

--- app/client/js/controllers/card-controller.js
+++ app/client/js/controllers/card-controller.js
@@ -3,17 +3,18 @@ app.controller('card-controller', ['$scope', '$resource', '$http', '$location','
 	function ($scope, $resource, $http, $location, $routeParams, $mdDialog) {
 
 		$scope.getcardResults = function() {
+			
 			$scope.setIdNum = $routeParams.setIdNum;
 
 	    	var url = "/card/" + $scope.setIdNum;
-			console.log(" cards url " + url);
+			console.log("cards url " + url);
 
 			$http.get(url).success(function(data){
 				var i = 0;
 				$scope.i = i;
 				$scope.resultCards = data[0];
+				
 				console.log("resultcards: " + $scope.resultCards);	
-				$scope.getSetDetails($scope.resultCards.setIdNum);
 			});
 		}
 
@@ -23,24 +24,23 @@ app.controller('card-controller', ['$scope', '$resource', '$http', '$location','
 		}
 
 
+		$scope.getSetDetails = function () {
+			$scope.setIdNum = $routeParams.setIdNum;
 
-
-		$scope.getSetDetails = function (setIdNum) {
-			var url = "/setDetails/" + setIdNum;
-			console.log(url);
+			var url = "/setDet/" + $scope.setIdNum;
+			console.log("hello"+url);
 			$http.get(url).success(function(data) {
-				$scope.setDetails = data;
+				$scope.setDets = data;
 			});
 		}
 
-		$scope.getRelatedSets = function(category) {
+		/*$scope.getRelatedSets = function(category) {
 			var url = "/relatedSets/" + category;
 			console.log(url);
 			$http.get(url).success(function(data){
 				$scope.relatedSets = data;
 			});
-		}
-
+		}*/
 
 
 
