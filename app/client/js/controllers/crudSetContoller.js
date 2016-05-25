3b098e69f731f14968416df5e480534244344404
anthonyabsher <anthonyabsher@gmail.com>
2016-05-25 12:14:24 -0700

    added delete

---
+++ app/client/js/controllers/crudSetContoller.js
@@ -0,0 +1,25 @@
+var app = angular.module('studybuddyApp');
+
+app.controller('crudSetCntrl', ['$scope', '$resource', '$http', '$location','$routeParams',
+	function ($scope, $resource, $http, $location, $routeParams) {
+
+		$scope.initNewSet = function() {
+			console.log('new set init');
+			var newSet = {};
+			newSet.setIdNum = 0;
+			newSet.Name = "lalala";
+			newSet.Author = "Chris";
+			newSet.Category = "test";
+			newSet.numCards = 0;
+			newSet.dateCreated = new Date();
+			$scope.set = newSet;
+		}
+
+		$scope.writeSet = function() {
+			console.log($scope.set);
+			$http.post('/createSet', $scope.set).success(function(data, status, headers, config) {
+				$scope.set.setIdNum = data.setIdNum;
+			});
+		}
+
+}]);
