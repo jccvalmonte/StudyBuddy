80a68807efe62bd02472691d193c35e435e6a9c2
dipaliv <vagal.dipali@gmail.com>
2016-05-28 11:15:48 -0700

    user session and password changes in mongodb

--- app/server.js
+++ app/server.js
@@ -2,31 +2,85 @@ var express           		= require ('express'),
 	app    			 		= express(),
 	bodyParser              = require('body-parser'),
 	mongoose                = require('mongoose'),
-	url 					= require('url');
-
-//initializing mongoose connection to the MongoDB database
-//route to database held in 'db.config'
-var mongoDBConnection = require('./db.config');
-mongoose.connect(mongoDBConnection.uri);
-console.log(mongoDBConnection.uri);
-
-//mongoose.connect('mongodb://anthony:absher@ds036069.mlab.com:36069/studybuddy');
+	url 					= require('url'),
+	cookieParser			= require('cookie-parser'),
+	http 					= require("http"),
+	uriUtil					= require("mongodb-uri"),
+	util 					= require('util'),
+	morgan 					= require('morgan'),
+	methodOverride 			= require('method-override'),
+	session 				= require('express-session'),
+	MongoStore 				= require('connect-mongo')(session),
+	crypto 					= require('crypto');
 
 //global variables to access the schema models
 var Sets;
 var Cards;
 
+/*const hmac = crypto.createHmac('sha256', 'a secret');
+console.log("hash is"+ hmac);*/
+
+//initializing mongoose connection to the MongoDB database
+//route to database held in 'db.config'
+
+//put config url behind file to hide passwords and username
+var mongoDBConnection = require('./db.config');
+mongoose.connect(mongoDBConnection.uri);
+console.log(mongoDBConnection.uri);
+
+var mongooseUri = uriUtil.formatMongoose(mongoDBConnection.uri);
+console.log("mongooseDB URI:" + mongooseUri);
+
 var idGen = 5;
 
-app.use(bodyParser());
-app.use(express.static('./'));
-app.use('/js', express.static('./client/js/controllers'));
-app.use('/images', express.static('./images'));
+app.set('port', process.env.PORT || 8080); //3000);
+app.use(morgan('combined'));
+//app.use(bodyParser());
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+//app.use('/js', express.static('./client/js/controllers'));
+//app.use('/images', express.static('./images'));
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } }; 
+mongoose.createConnection(mongooseUri, options);
+
+app.use(session({ 
+		secret: 'keyboard cat',
+		store: new MongoStore({ 
+			mongooseConnection: mongoose.connection,
+			collection: 'sessions'
+		})
+}));
+
+console.log('Sending connecting request with Mongo db');
+mongoose.connection.on('error', function() {
+	console.log("problems connecting to the MongoDB server");
+});
 
 mongoose.connection.on('open', function(){
-	console.log('DB connection established!');
+	console.log("After connecting to Mongo");
 
 	var Schema = mongoose.Schema;
+
+	var AccountSchema = new Schema({
+		email: String,
+		firstName: String,
+		lastName: String,
+		//password: String,
+		username: String,
+		dob: String,
+		hashed_pwd: String
+	},
+	{collection: 'accounts'}
+	);	
+	Accounts = mongoose.model('Accounts', AccountSchema);
 
 	var CardSetSchema = new Schema({
 		setIdNum: String,
@@ -34,15 +88,16 @@ mongoose.connection.on('open', function(){
 		Category: String,
 		numCards : Number,
 		Author: String,
-		DateCreated: Date
+		DateCreated: Date,
+		email: String
 	},
 	{collection: 'sets'}
 	);
-
 	Sets = mongoose.model('Sets', CardSetSchema);
 	
 	var CardListSchema = new Schema({
 		setIdNum: String,
+		Author: String,
 		cards : [{
 			cardId: Number,
 			front : String,
@@ -51,48 +106,75 @@ mongoose.connection.on('open', function(){
 	},
 	{collection: 'cards'}
 	);
-
 	Cards = mongoose.model('Cards', CardListSchema);
-
-	var AccountSchema = new Schema({
-		email: String,
-		firstName: String,
-		lastName: String,
-		password: String,
-		username: String,
-		dob: String
-	},
-	{collection: 'accounts'}
-	);
-	
-	Accounts = mongoose.model('Accounts', AccountSchema);
 	
 	console.log('Models Created!');
 });
 
+function displayDBError(err){
+	if (err) { 
+		console.log("error 1: " + err);
+		for (p in err) {
+			console.log(p + ":" + err[p]);
+		}
+	}
+	else {
+		console.log("no errors querying the db");
+	}
+}
 
 
-//REST API
-/*app.get('/', function (req, res){
-	res.sendfile(__dirname + '/client/views/index.html');
-});
-app.get('/card', function (req, res){
-	res.sendfile(__dirname + '/client/views/card.html');
-});
+function retrieveUserIdWithPwd(req, res, query) {
+	console.log("calling retrieve user Id");
+	var query = Accounts.findOne(query);
+	query.exec(function (err, user) {
+		if (!user) {
+			req.session.user = undefined;
+			res.sendStatus(404);
+			return;
+		}
+		else {
+			//req.session.regenerate(function(){
+				var pwd = req.query.password;
+				console.log("pwd is: "+ pwd);
 
-app.get('/signup', function (req, res){
-	res.sendfile(__dirname + '/client/views/signUp.html');
-});
+				var hashedPwd = crypto.createHash('sha256').update(pwd).digest('base64').toString();
+				
+				if (hashedPwd == user.hashed_pwd) {
+				req.session.user = user.id.valueOf();
+				req.session.username = user.username;
+				req.session.email = user.email;
+				console.log('user information is correct');
+			}
+			else {
+				console.log('incorrect password');
+			}
+			
+		}
+		if (err) {
+			console.log("errors accessing users");
+		}
+		else {
+			console.log("----------->user info:" + user);
+			res.sendStatus(200);
+			//res.json(req.session.user);
+		}
+	});	
+}
 
-app.get('/home', function (req, res){
-	res.sendfile(__dirname + '/home.html');
-});
 
-*/
+console.log("before defining app static route");
+app.use(express.static('./'));
 
 
-//app.get('/api/flashcard_sets', flashcardsetsController.list);
-//app.post('/api/flashcard_sets', flashcardsetsController.create);
+app.get('/checklogin/:username', function(req, res) {
+	console.log("making a login request to server");
+	console.log(req);
+	var id = req.params.username;
+
+	retrieveUserIdWithPwd(req, res, {username: id});
+
+}); 
 
 app.get('/homeSets', function(req, res){
 	
@@ -103,9 +185,6 @@ app.get('/homeSets', function(req, res){
 			res.json(found);
 	});
 });
-
-
-
 
 app.get('/relatedSets/:category', function(req, res) {
 
@@ -131,9 +210,6 @@ app.get('/setDet/:setIdNum', function(req, res) {
 	});
 });
 
-
-
-
 app.get('/card/:setIdNum', function(req, res) {
 
 	var searchrequest = req.params.setIdNum;
@@ -146,22 +222,7 @@ app.get('/card/:setIdNum', function(req, res) {
 		//console.log(res.json);
 		res.json(found); // return all cards in JSON format
 	});
-});	
-
-app.get('/getAccount/:email/:password', function(req, res) {
-
-	var email = req.params.email;
-	var password = req.params.password;
-
-	Accounts.find({email: email}, {password: password}, function(err, found) {
-     	// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-     	if (err)
-     		res.send(err)
-     	else
-        //console.log(res.json);
-        res.json(found); // return all accounts in JSON format
-    });
-});          
+});	         
 
 app.post('/signup', function(req, res) {
 
@@ -220,6 +281,10 @@ app.delete('userSets/delete/:setId', function(req,res){
 });
 
 
-app.listen(3000, function() {
+/*app.listen(3000, function() {
 	console.log('Server listening on port 3000...');
-})
+})*/
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});	
+console.log("after callintg http: createServer");
